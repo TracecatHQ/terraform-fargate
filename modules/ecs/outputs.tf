f3d4cbc5a7e05cb4c1e3df24ccfdaaad4a291ea3
topher-lo <46541035+topher-lo@users.noreply.github.com>
2025-07-05 12:30:37 -0700

    fix outputs if snapshot latest us encrypted

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -31,13 +31,13 @@ output "local_dns_namespace" {
 # Outputs
 
 output "latest_core_snapshot_encrypted" {
-  value       = var.restore_from_snapshot ? data.aws_db_snapshot.core_snapshots[0].encrypted : null
-  description = "Whether the latest core database snapshot is encrypted"
+  value       = var.restore_from_snapshot && var.core_db_snapshot_name == null ? try(data.aws_db_snapshot.core_snapshots[0].encrypted, null) : null
+  description = "Whether the latest core database snapshot is encrypted (only available when using automatic snapshot selection)"
 }
 
 output "latest_temporal_snapshot_encrypted" {
-  value       = var.restore_from_snapshot ? data.aws_db_snapshot.temporal_snapshots[0].encrypted : null
-  description = "Whether the latest temporal database snapshot is encrypted"
+  value       = var.restore_from_snapshot && var.temporal_db_snapshot_name == null && !var.disable_temporal_autosetup ? try(data.aws_db_snapshot.temporal_snapshots[0].encrypted, null) : null
+  description = "Whether the latest temporal database snapshot is encrypted (only available when using automatic snapshot selection)"
 }
 
 output "s3_attachments_bucket_name" {
