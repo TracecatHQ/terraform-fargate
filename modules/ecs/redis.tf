82d5fd07e5379ed5481b18fd15644619d47daf02
topher-lo <46541035+topher-lo@users.noreply.github.com>
2025-07-29 21:02:39 -0700

    fix: Add required default user to ElastiCache user group
    AWS ElastiCache requires every user group to contain a user named 'default'.
    Added a disabled default user with no access (off ~* -@all) to satisfy this requirement
    while maintaining security.

--- modules/ecs/redis.tf
+++ modules/ecs/redis.tf
@@ -4,6 +4,17 @@ resource "aws_elasticache_subnet_group" "redis" {
   subnet_ids = var.private_subnet_ids
 }
 
+# Default user (required by AWS ElastiCache)
+resource "aws_elasticache_user" "default" {
+  user_id       = "default-user-tracecat"
+  user_name     = "default"  # Must be named "default"
+  engine        = "REDIS"
+  access_string = "off ~* -@all"  # Disabled user with no access
+  authentication_mode {
+    type = "no-password-required"
+  }
+}
+
 # IAM-auth user + user-group
 resource "aws_elasticache_user" "iam_user" {
   user_id       = "tracecat-iam-user"
@@ -16,7 +27,10 @@ resource "aws_elasticache_user" "iam_user" {
 resource "aws_elasticache_user_group" "redis" {
   user_group_id = "tracecat-users"
   engine        = "REDIS"
-  user_ids      = [aws_elasticache_user.iam_user.user_id]
+  user_ids      = [
+    aws_elasticache_user.default.user_id,
+    aws_elasticache_user.iam_user.user_id
+  ]
 }
 
 # The replication group (single-node, TLS & KMS encryption are ON by default in Redis 7)
